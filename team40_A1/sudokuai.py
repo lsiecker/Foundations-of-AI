648b5ea82df39c4413dcb8f97cc0099bad66dcb4
Luc Siecker <lsiecker@quicknet.nl>
2022-11-22 13:41:24 +0100

    Added evalutaion of a move and setting propose_move

--- team40_A1/sudokuai.py
+++ team40_A1/sudokuai.py
@@ -25,9 +25,8 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
                 i,j = SudokuBoard.f2rc(board, k)
                 if board.get(i,j) == SudokuBoard.empty:
                     emptyCells.append([i,j])
+            print(emptyCells)
             return emptyCells
-        
-        # TODO: implement a way to generate all the legal moves from a given game state / board position
 
         def checkColumn(i, j, value):
             for col in range(N):
@@ -49,6 +48,31 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
                     if game_state.board.get(x+col, y+row) == value:
                         return False
             return True
+        
+        def completeColumn(i, j):
+            for col in range(N):
+                if game_state.board.get(col, j) == SudokuBoard.empty \
+                        and col != i:
+                    return False
+            return True
+        
+        def completeRow(i, j):
+            for row in range(N):
+                if game_state.board.get(i, row) == SudokuBoard.empty \
+                        and row != j:
+                    return False
+            return True
+        
+        def completeBlock(i, j):
+            x = i - i % game_state.board.m
+            y = j - j % game_state.board.n
+            for col in range(game_state.board.m):
+                for row in range(game_state.board.n):
+                    if game_state.board.get(x+col, y+row) == SudokuBoard.empty \
+                            and x+col != i \
+                            and y+row != j:
+                        return False
+            return True
 
         def possible(i, j, value):
             return game_state.board.get(i, j) == SudokuBoard.empty \
@@ -60,12 +84,21 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
         all_moves = [Move(cell[0], cell[1], value) for cell in checkEmpty(game_state.board, N)
                      for value in range(1, N+1) if possible(cell[0], cell[1], value)]
 
+        
         # TODO: Implement an evaluation function that assigns a numerical score to any game state
+        def evaluate(move):
+            return completeRow(move.i, move.j) + completeColumn(move.i, move.j)+ completeBlock(move.i, move.j)
 
         # TODO: Implement a variant of the minimax tree search algorithm
         # --> Assign the best score at the moment to self.propose_move(), and update this every time
 
+        best_move = random.choice(all_moves)
+
         while True:
-            self.propose_move(random.choice(all_moves))
+            if evaluate(all_moves[0]) < evaluate(best_move):
+                all_moves.remove[0]
+            else:
+                best_move = all_moves[0]
+            self.propose_move(best_move)
             time.sleep(0.2)
 
